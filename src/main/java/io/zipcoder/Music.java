4fe6f77cd193aadb4921ca9f01708e8cee0fdaed
vle <obentoka@gmail.com>
2020-02-13 22:30:11 -0500

    finshed with extra logic

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,44 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int leastButtonPressed = 0;
+        int firstSelectionIndex = 0;
+        int secondSelectionIndex = 0;
+        int distanceFromFristIndex = 0;
+        int distanceFromSencondIndex = 0;
+        boolean isFirstTimeSelection = true;
+
+        for (int i = 0; i < playList.length; i++) {
+            if(playList[i].equals(selection) && isFirstTimeSelection){
+                isFirstTimeSelection = false;
+                firstSelectionIndex = i;
+            } else if(playList[i].equals(selection) && !isFirstTimeSelection){
+                secondSelectionIndex = i;
+            }
+        }
+        System.out.println(isFirstTimeSelection + " " +secondSelectionIndex);
+        if(!isFirstTimeSelection && secondSelectionIndex != 0){
+            firstSelectionIndex = firstSelectionIndex % (playList.length -1);
+            secondSelectionIndex = secondSelectionIndex % (playList.length -1);
+            distanceFromFristIndex = firstSelectionIndex - startIndex;
+
+            if(secondSelectionIndex == 0){
+                distanceFromSencondIndex = startIndex - secondSelectionIndex + 1;
+            }else {
+                distanceFromSencondIndex = secondSelectionIndex - startIndex;
+            }
+
+            if(distanceFromFristIndex < distanceFromSencondIndex){
+                leastButtonPressed = distanceFromFristIndex;
+            }else {
+                leastButtonPressed = distanceFromSencondIndex;
+            }
+        }else {
+            leastButtonPressed = firstSelectionIndex - startIndex;
+            if(firstSelectionIndex == 0 && (startIndex - firstSelectionIndex) < leastButtonPressed){
+                leastButtonPressed = startIndex - firstSelectionIndex;
+            }
+        }
+        return leastButtonPressed;
     }
 }
